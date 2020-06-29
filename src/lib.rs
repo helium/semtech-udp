9a7007581f94781f777cef22cbfa20ced51a54ac
lthiery <me@louisthiery.com>
2020-06-29 09:32:53 -0700

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -52,48 +52,47 @@ impl Packet {
     pub fn parse(buffer: &[u8], num_recv: usize) -> std::result::Result<Packet, Box<dyn stdError>> {
         if buffer[0] != PROTOCOL_VERSION {
             Err(Error::InvalidProtocolVersion.into())
-        } else {
-            if let Ok(id) = Identifier::try_from(buffer[3]) {
-                Ok(Packet {
-                    // all packets have random_token
-                    random_token: random_token(buffer),
-                    // only PULL_DATA nad PUSH_DATA have MAC_IDs
-                    gateway_mac: match id {
-                        Identifier::PullData | Identifier::PushData | Identifier::TxAck => {
-                            Some(gateway_mac(buffer))
-                        }
-                        _ => None,
-                    },
-                    data: match id {
-                        Identifier::PullData => PacketData::PullData,
-                        Identifier::PushData => {
-                            let json_str = std::str::from_utf8(&buffer[12..num_recv])?;
-                            PacketData::PushData(serde_json::from_str(json_str)?)
-                        }
-                        Identifier::PullResp => {
-                            let json_str = std::str::from_utf8(&buffer[4..num_recv])?;
-                            PacketData::PullResp(serde_json::from_str(json_str)?)
-                        }
-                        Identifier::PullAck => PacketData::PullAck,
-                        Identifier::PushAck => PacketData::PushAck,
-                        Identifier::TxAck => PacketData::TxAck,
-                    },
-                })
-            } else {
-                Err(Error::InvalidIdentifier.into())
-            }
+        } else if let Ok(id) = Identifier::try_from(buffer[3]) {
+            Ok(Packet {
+                // all packets have random_token
+                random_token: random_token(buffer),
+                // only PULL_DATA nad PUSH_DATA have MAC_IDs
+                gateway_mac: match id {
+                    Identifier::PullData | Identifier::PushData | Identifier::TxAck => {
+                        Some(gateway_mac(buffer))
+                    }
+                    _ => None,
+                },
+                data: match id {
+                    Identifier::PullData => PacketData::PullData,
+                    Identifier::PushData => {
+                        let json_str = std::str::from_utf8(&buffer[12..num_recv])?;
+                        PacketData::PushData(serde_json::from_str(json_str)?)
+                    }
+                    Identifier::PullResp => {
+                        let json_str = std::str::from_utf8(&buffer[4..num_recv])?;
+                        PacketData::PullResp(serde_json::from_str(json_str)?)
+                    }
+                    Identifier::PullAck => PacketData::PullAck,
+                    Identifier::PushAck => PacketData::PushAck,
+                    Identifier::TxAck => PacketData::TxAck,
+                },
+            })
+        }
+        else {
+            Err(Error::InvalidIdentifier.into())
         }
     }
 
     pub fn serialize(self, buffer: &mut [u8]) -> std::result::Result<u64, Box<dyn stdError>> {
         let mut w = Cursor::new(buffer);
-        w.write(&[
+        w.write_all(&[
             PROTOCOL_VERSION,
             (self.random_token >> 8) as u8,
             self.random_token as u8,
         ])?;
 
-        w.write(&[match &self.data {
+        w.write_all(&[match &self.data {
             PacketData::PushData(_) => Identifier::PushData,
             PacketData::PushAck => Identifier::PushAck,
             PacketData::PullData => Identifier::PullData,
@@ -103,15 +102,12 @@ impl Packet {
         } as u8])?;
 
         if let Some(mac) = self.gateway_mac {
-            w.write(mac.bytes())?;
+            w.write_all(mac.bytes())?;
         };
 
-        match self.data {
-            PacketData::PushData(data) => {
-                w.write(&serde_json::to_string(&data)?.as_bytes())?;
-            }
-            _ => (),
-        };
+        if let PacketData::PushData(data)  = self.data {
+            w.write_all(&serde_json::to_string(&data)?.as_bytes())?;
+        }
         Ok(w.position())
     }
 }
