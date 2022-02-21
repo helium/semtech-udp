c4da82276c7e25923f3cf3650ae5b6e4f4e03ea5
lthiery <thiery.louis@gmail.com>
2022-02-21 10:24:00 -0800

    improve txpk interface

--- examples/gwmp-mux.rs
+++ examples/gwmp-mux.rs
@@ -84,11 +84,11 @@ async fn host_and_mux(cli: Opt, shutdown_signal: triggered::Listener) -> Result 
                 return Ok(());
             },
             server_event = client_rx.recv() => {
-                if let Some((mut packet, mac)) = match server_event {
+                let mut to_send = None;
+                match server_event {
                     ServerEvent::UnableToParseUdpFrame(error, buf) => {
                         error!(logger, "Semtech UDP Parsing Error: {error}");
                         error!(logger, "UDP data: {buf:?}");
-                        None
                     }
                     ServerEvent::NewClient((mac, addr)) => {
                         info!(logger, "New packet forwarder client: {mac}, {addr}");
@@ -106,29 +106,24 @@ async fn host_and_mux(cli: Opt, shutdown_signal: triggered::Listener) -> Result 
                             handles.push(handle);
                         }
                         mux.insert(mac, clients);
-                        None
                     }
                     ServerEvent::UpdateClient((mac, addr)) => {
                         info!(logger, "Mac existed, but IP updated: {mac}, {addr}");
-                        None
                     }
                     ServerEvent::PacketReceived(rxpk, mac) => {
                         info!(logger, "From {mac} received uplink: {rxpk}");
-
-                        Some((push_data::Packet::from_rxpk(rxpk), mac))
-
+                        to_send = Some(push_data::Packet::from_rxpk(mac, rxpk));
                     }
                     ServerEvent::StatReceived(stat, mac) => {
                         info!(logger, "From {mac} received stat: {stat:?}");
-                        Some((push_data::Packet::from_stat(stat), mac))
+                        to_send = Some(push_data::Packet::from_stat(mac, stat));
                     }
                     ServerEvent::NoClientWithMac(_packet, mac) => {
                         warn!(logger, "Downlink sent but unknown mac: {mac:?}");
-                        None
                     }
-                } {
-                    if let Some(clients) = mux.get_mut(&mac) {
-                        packet.gateway_mac = mac;
+                }
+                if let Some(packet) = to_send {
+                    if let Some(clients) = mux.get_mut(&packet.gateway_mac) {
                         for sender in clients {
                             debug!(logger, "Forwarding Uplink");
                             let logger = logger.clone();
