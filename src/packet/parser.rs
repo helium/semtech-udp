a3998165327d6a9d4e13db56db88f30c265a7d17
lthiery <thiery.louis@gmail.com>
2023-08-16 13:50:32 -0700

    use the const instead of a magic number

--- src/packet/parser.rs
+++ src/packet/parser.rs
@@ -13,7 +13,10 @@ fn random_token(buffer: &[u8]) -> u16 {
 
 pub fn gateway_mac(buffer: &[u8]) -> Result<MacAddress, ParseError> {
     if buffer.len() < GATEWAY_MAC_LEN {
-        Err(ParseError::InvalidPacketLength(buffer.len(), 8))
+        Err(ParseError::InvalidPacketLength(
+            buffer.len(),
+            GATEWAY_MAC_LEN,
+        ))
     } else {
         Ok(MacAddress::new(
             buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7],
